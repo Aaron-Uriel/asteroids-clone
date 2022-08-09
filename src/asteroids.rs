224f047e373cfbd235841ca7cf8730644e363a7b
Aaron-Uriel <therialaaronuriel@gmail.com>
2022-08-09 12:25:03 -0500

    Asteroids use impulse instead of velocity

--- src/asteroids.rs
+++ src/asteroids.rs
@@ -20,16 +20,18 @@ impl Plugin for AsteroidsPlugin {
 fn spawn_asteroids_system(mut commands: Commands, ascii: Res<AsciiSheet>) {
     use std::f32;
     use rand::prelude::*;
-    use rand::distributions::Uniform;
+    use rand::distributions::{Uniform, Bernoulli};
 
     let mut rng = rand::thread_rng();
     let coordinates_range: Uniform<f32> = Uniform::from(-100.0..100.0);
-    let velocity_range: Uniform<f32> = Uniform::from(10.0..100.0);
-    let angle_range: Uniform<f32> = Uniform::from(0.0..(2.0 * f32::consts::PI));
+    let impulse_range: Uniform<f32> = Uniform::from(0.25..0.5);
+    let boolean_rand = Bernoulli::new(0.5).unwrap();
 
     for i in 0..ASTEROIDS_LIMIT {
-        let abs_vel = velocity_range.sample(&mut rng);
-        let angle = angle_range.sample(&mut rng);
+        let impulse = Vec2::new(
+            impulse_range.sample(&mut rng) * if boolean_rand.sample(&mut rng) { -1.0 } else { 1.0 },
+            impulse_range.sample(&mut rng) * if boolean_rand.sample(&mut rng) { -1.0 } else { 1.0 }
+        );
 
         let new_asteroid = spawn_ascii_sprite(
             &mut commands,
@@ -38,13 +40,12 @@ fn spawn_asteroids_system(mut commands: Commands, ascii: Res<AsciiSheet>) {
             Color::rgb(0.4, 0.5, 0.9),
             BASE_SPRITE_SIZE
         );
-
         commands.entity(new_asteroid)
-            .insert_bundle(TransformBundle::from(Transform {
-                translation: [coordinates_range.sample(&mut rng), coordinates_range.sample(&mut rng), 700.0].into(),
-                rotation: Quat::from_rotation_z(angle),
-                ..Default::default()
-            }))
+            .insert_bundle(TransformBundle::from(Transform::from_xyz(
+                coordinates_range.sample(&mut rng),
+                coordinates_range.sample(&mut rng),
+                coordinates_range.sample(&mut rng)
+            )))
             .insert(Asteroid)
             .insert(RigidBody::Dynamic)
             .insert(Damping {
@@ -55,11 +56,8 @@ fn spawn_asteroids_system(mut commands: Commands, ascii: Res<AsciiSheet>) {
                 BASE_SPRITE_SIZE.x / 2.0, 
                 BASE_SPRITE_SIZE.y / 2.0
             ))
-            .insert(Velocity {
-                linvel: Vec2::new(
-                    abs_vel * angle.cos(),
-                    abs_vel * angle.sin()
-                ),
+            .insert(ExternalImpulse {
+                impulse: impulse,
                 ..Default::default()
             })
             .insert(Name::new(format!("Asteroid {}", i)));
